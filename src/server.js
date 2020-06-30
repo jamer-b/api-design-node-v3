d4fe8b544bf1e8800758ae01579ac254d3e8aaa6
JAlfonsoMerino <juanalfonsomerino@gmail.com>
2020-06-30 11:23:20 +0200

    leccion1

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ import express from 'express'
 import { json, urlencoded } from 'body-parser'
 import morgan from 'morgan'
 import cors from 'cors'
+import { connect } from './utils/db'
+import config from './config/testing'
 
 export const app = express()
 
@@ -12,4 +14,14 @@ app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
 
-export const start = () => {}
+export const start = async () => {
+  try {
+    await connect()
+
+     app.listen(3000, () => {
+     console.log(`REST API on http://localhost:${3000}/api`)
+      })
+  } catch (e) {
+    console.error(e)
+  }
+}
